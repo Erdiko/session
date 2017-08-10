df7ff98214d02bdce00e479021eaa3983daf48fd
Mauricio Pineda <pinedamg@gmail.com>
2017-08-10 09:08:18 -0300

    Started PHPUnit

---
+++ tests/phpunit/SessionDriverFileTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace tests\phpunit;
+
+require_once dirname(__DIR__) . '/ErdikoTestCase.php';
+
+use erdiko\session\Session;
+use tests\ErdikoTestCase;
+
+class SessionTest extends ErdikoTestCase
+{
+
+	public function setUp()
+	{
+	}
+
+}
